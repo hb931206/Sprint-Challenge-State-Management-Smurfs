b723adac546bd18998e8c05af97f1bf279bebe09
Henry Brophy <henrybrophy@henrys-mbp.lan>
2020-05-01 20:36:34 -0400

    Working with 3 cases before I asked James why I couldn't just use 1 case.

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,7 +1,19 @@
 import axios from "axios";
 
-// export const getSmurf=()=>(dispatch)=>{
-//     dispatch({})
-// }
+export const FETCHING_SMURFS_START = "FETCHING_SMURFS_START";
+export const FETCHING_SMURFS_SUCCESS = "FETCHING_SMURFS_SUCCESS";
+export const FETCHING_SMURFS_FAILED = "FETCHING_SMURFS_FAILED";
 
-axios.get("http://localhost:3333/smurfs");
+export const getSmurfs = () => (dispatch) => {
+  dispatch({ type: FETCHING_SMURFS_START });
+
+  axios
+    .get("http://localhost:3333/smurfs")
+    .then((res) => {
+      console.log(res);
+      dispatch({ type: FETCHING_SMURFS_SUCCESS, payload: res.data.name });
+    })
+    .catch((err) => {
+      dispatch({ type: FETCHING_SMURFS_FAILED, payload: "Failed" });
+    });
+};
--- smurfs/src/reducers/smurfReducer.js
+++ smurfs/src/reducers/smurfReducer.js
@@ -1,9 +1,17 @@
+import {
+  FETCHING_SMURFS_START,
+  FETCHING_SMURFS_SUCCESS,
+  FETCHING_SMURFS_FAILED,
+} from "../actions/index";
+
 const initialState = [{ name: "Brainey", age: "200", height: "5cm", id: 0 }];
 
 export const smurfReducer = (state = initialState, action) => {
   switch (action.type) {
-    // case FETCHING_SMURF_SUCCESS:
-    //   return {};
+    case FETCHING_SMURFS_START:
+      return { ...state };
+    case FETCHING_SMURFS_SUCCESS:
+      return {};
     default:
       return state;
   }
